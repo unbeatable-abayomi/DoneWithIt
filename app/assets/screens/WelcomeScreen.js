1f9df8086b690a6abb8b2d95e578ae543cdc0891
unbeatable-abayomi <igwubor@outlook.com>
2021-03-28 09:04:54 +0100

    improvedLoginPage

--- app/assets/screens/WelcomeScreen.js
+++ app/assets/screens/WelcomeScreen.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Image, ImageBackground ,StyleSheet, Text, View} from 'react-native';
+import AppButton from '../../components/AppButton';
 
 function WelcomeScreen(props) {
     return (
-        <ImageBackground 
+        <ImageBackground blurRadius={10}
         style={styles.background} 
         source={require("/Users/abayomiigwubor/Projects/expoProjects/DoneWithIt/app/assets/background.jpg")}>
       <View style={styles.logoContainer}>
           <Image style={styles.logo} source={require("/Users/abayomiigwubor/Projects/expoProjects/DoneWithIt/app/assets/logo-red.png")}></Image> 
-       <Text>Sell What you Don't Want</Text>  
+       <Text style={styles.tagline}>Sell What you Don't Want</Text>  
       </View>
        {/* <Image style={styles.logo} source={require("/Users/abayomiigwubor/Projects/expoProjects/DoneWithIt/app/assets/logo.jpeg")}></Image> 
        <Text>Sell What you Don't Want</Text>     */}
-<View style={styles.loginButton}></View>
-<View style={styles.registerButton}></View>
+       <View style={styles.buttonContanier}>
+
+       <AppButton title="Login"/>
+       <AppButton title="Register" color="secondary"/>
+      {/* <View style={styles.registerButton}></View> */}
+       </View>
+       {/* <AppButton title='Login'/>
+<View style={styles.registerButton}></View> */}
+{/* <View style={styles.loginButton}></View> */}
         </ImageBackground>
     );
 }
@@ -24,26 +32,36 @@ const styles = StyleSheet.create({
         justifyContent: "flex-end",
         alignItems: 'center'
     },
-    loginButton :{
-        width :"100%",
-        height : 70,
-        backgroundColor : "#1d3557"
+    buttonContanier:{
+        padding : 20,
+        width : "100%" 
     },
+   
+    // loginButton :{
+    //     width :"100%",
+    //     height : 70,
+    //     backgroundColor : "#1d3557"
+    // },
     logo : {
         width : 100,
         height: 100,
         borderRadius: 30
         
     },
-    registerButton :{
-        width :"100%",
-        height : 70,
-        backgroundColor : "#fc5c65"
-    },
+    // registerButton :{
+    //     width :"100%",
+    //     height : 70,
+    //     backgroundColor : "#fc5c65"
+    // },
     logoContainer:{
         position : "absolute",
         top: 70,
         alignItems : 'center'
+    },
+    tagline:{
+        fontSize : 25,
+        fontWeight : '600',
+        paddingVertical : 20,
     }
 })
 
